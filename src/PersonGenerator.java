77520626784e70a42ef229c6143355bf2d142fc9
Olegus85 <Olegus85@yandex.ru>
2024-05-22 19:20:37 +0300

    сделал поля PersonGenerator статическими

---
+++ src/PersonGenerator.java
@@ -0,0 +1,9 @@
+public class PersonGenerator implements Generator<Person>{
+    private static NameGenerator nameGenerator = new NameGenerator();
+    private static AgeGenerator ageGenerator = new AgeGenerator();
+
+    @Override
+    public Person generate() {
+        return new Person(nameGenerator.generate(), ageGenerator.generate());
+    }
+}
