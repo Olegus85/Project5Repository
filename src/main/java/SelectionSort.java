565f87b79f252b3471fa1cba39e5a6c7f62f7dd2
Olegus85 <Olegus85@yandex.ru>
2024-06-04 16:43:49 +0300

    подправил оформление

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class SelectionSort implements SortAlgorithm<Person>{
+public class SelectionSort implements SortAlgorithm<Person> {
     @Override
     public Generator<Person> getGenerator() {
         return new PersonGenerator();
@@ -10,17 +10,19 @@ public class SelectionSort implements SortAlgorithm<Person>{
     public Person[] sort(Person[] arrayOfPersons) {
         System.out.println("Before Selection sorting: " + Arrays.toString(arrayOfPersons));
 
-        for(int i=0;i<arrayOfPersons.length;i++){
-            Person min=arrayOfPersons[i];
-            int minIndex=i;
-            for (int j=i+1;j<arrayOfPersons.length;j++){
-                if(min.getAge()>arrayOfPersons[j].getAge()){
-                    min=arrayOfPersons[j];
-                    minIndex=j;
+        for (int i = 0; i < arrayOfPersons.length; i++) {
+
+            Person min = arrayOfPersons[i];
+
+            int minIndex = i;
+            for (int j = i + 1; j < arrayOfPersons.length; j++) {
+                if (min.getAge() > arrayOfPersons[j].getAge()) {
+                    min = arrayOfPersons[j];
+                    minIndex = j;
                 }
             }
-            arrayOfPersons[minIndex]=arrayOfPersons[i];
-            arrayOfPersons[i]=min;
+            arrayOfPersons[minIndex] = arrayOfPersons[i];
+            arrayOfPersons[i] = min;
         }
 
         System.out.println("After Selection sorting: " + Arrays.toString(arrayOfPersons));
