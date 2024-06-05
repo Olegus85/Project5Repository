338f098fb3c8c2c6e11698d2267a745719e68f49
Olegus85 <Olegus85@yandex.ru>
2024-06-05 20:45:43 +0300

    создали класс PersonController2

---
+++ module/src/main/java/com/example/demo/PersonController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.olegus.types.Person;
+import org.springframework.stereotype.Service;
+import packege.PersonGenerator;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class PersonController {
+    public List<Person> getGeneratedPersonList() {
+        PersonGenerator personGenerator = new PersonGenerator();
+        return Stream.iterate(personGenerator.generate(), person -> personGenerator.generate())
+                .limit(new Random().nextInt(10)).collect(Collectors.toList());
+    }
+}
